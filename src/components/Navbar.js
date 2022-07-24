e8d44ac922a810071dd99965b1468649d775c86e
Olivia Adongo <akinyioliviatove@gmail.com>
2022-07-24 19:22:21 +0300

    Navigation checked

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,8 +10,13 @@ const linkStyles = {
   color: "white",
 };
 
-function Navbar({ setIsLoggedIn }) {
+function Navbar({ onLogout }) {
   const history = useHistory();
+
+  function handleClick() {
+    onLogout();
+    history.push("/login");
+  }
 
   function handleLogout() {
     setIsLoggedIn(false);
@@ -53,9 +58,10 @@ function Navbar({ setIsLoggedIn }) {
       >
         Login
       </NavLink>
-      <button onClick={handleLogout}>Logout</button>
+      <button onClick={handleClick}>Logout</button>
     </div>
   );
 }
+
 
 export default Navbar;
